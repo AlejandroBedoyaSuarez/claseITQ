191a162089f4caf4b17c3fd4cd8e0ba7413917da
Alejandro <addrriel@hotmail.com>
2024-11-05 23:33:25 -0500

    Avance

--- deber6.java
+++ deber6.java
@@ -10,7 +10,9 @@ public class deber6 {
         System.out.println(
                 "Programa que permita dar las respuestas de las variables x and y en un sistema de ecuaciones lineales");
         
+        //hacemos el objeto para el scan del usuario
         Scanner ecuaciones= new Scanner(System.in);
+        //hacemos escribir las variables hasta la F
         System.out.println("Escriba el valor a : ");
         double valorA= ecuaciones.nextDouble();
         System.out.println("Escriba el valor b : ");
@@ -24,16 +26,25 @@ public class deber6 {
         System.out.println("Escriba el valor f : ");
         double valorF = ecuaciones.nextDouble();
         
+        //Operación numerador X
         double resultadoX = valorC*valorE-valorB*valorF;
+        //Operación denominador X
         double resultadoX2 = valorA*valorE-valorB*valorD;
+        //Resultado de X
         double resultadoXtotal= resultadoX/resultadoX2;
 
+        //Operación numerador Y
         double resultadoY = valorA*valorF-valorC*valorD;
+        //Operación denominador Y
         double resultadoY2 = valorA*valorE-valorB*valorD;
+        //Resultado de Y
         double resultadoYtotal = resultadoY/resultadoY2;
         
+        //Respuesta al usuario
         System.out.println("El resultado de x es :" + resultadoXtotal + " , además el valor de y es : " + resultadoYtotal);
 
+       
+        //Cerramos ciclo
         ecuaciones.close();
     }
 }
